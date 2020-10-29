265fcc7384e5ea436cfd823eb9e3e629515be37e
dekokos <dekoxingyun@gmail.com>
2020-10-29 13:53:16 +0300

    css/js/img для новых страниц и обновления старых

--- design/src/blocks/product-item/product-item.js
+++ design/src/blocks/product-item/product-item.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
    let productItem = document.querySelectorAll('.product-item');
     productItem && productItem.forEach(function(el) {
         let video = el.querySelector('.product-item__video video');
-        if (video) {
+        if (video && window.innerWidth >= 992) {
             el.addEventListener('mouseenter', function() {
                 // console.log('enter');
                 el.classList.add('is-video');
