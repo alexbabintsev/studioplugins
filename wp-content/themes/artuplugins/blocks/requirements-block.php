e5caffdd424b13d0503a5fbb5125ad8d894f9287
Michil888 <michil888@mail.ru>
2020-10-31 15:43:34 +0300

    перестройка структуры блока requirements

--- wp-content/themes/artuplugins/blocks/requirements-block.php
+++ wp-content/themes/artuplugins/blocks/requirements-block.php
@@ -1,13 +1,15 @@
 <section class="requirements <?= esc_attr($block['className'])?>">
     <div class="container">
-        <div class="requirements-inner" style="background-color: <?= get_field('main_color')?>; background-image: url(<?= get_theme_file_uri( '/img/requirements/bg-blue.svg' )?>)">
+        <div class="requirements-inner" style="background-color: <?= get_field('main_color')?>; background-image: url(<?= get_field('background_image')?>)">
             <h2 class="requirements-title title"><?= get_field('title');?></h2>
             <div class="requirements-grid">
                 <?php foreach (get_field('blocks') as $block):?>
                     <div class="requirements-item">
                         <div class="requirements-item__ic-container">
                             <div class="requirements-item__ic-wrapper">
-                                <div class="requirements-item__ic"><img src="<?= $block['image']?>"></div>
+                                <?php foreach ($block['images'] as $image):?>
+                                <div class="requirements-item__ic"><img src="<?= $image?>"></div>
+                                <?php endforeach;?>
                             </div>
                         </div>
                         <div class="requirements-item__text"><?= $block['text']?></div>
